f9c5ee365b982fbf9d040455d3cc920e0f3f7516
wired_less_maker <70153663+sebastianttr@users.noreply.github.com>
2024-07-29 14:24:25 -0500

    Pushed text files used for processing

---
+++ prediction_input/bbc/schema.sql
@@ -0,0 +1,9 @@
+
+
+CREATE SEQUENCE id_sequence INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1034 NO CYCLE;
+
+CREATE TABLE prediction_input(id VARCHAR UNIQUE, articleId INTEGER UNIQUE, "timestamp" VARCHAR, "close" DOUBLE, relevance DOUBLE, sentimentPositive DOUBLE, sentimentNegative DOUBLE, sentimentNeutral DOUBLE);
+
+
+
+
---
+++ prediction_input/ft/load.sql
@@ -0,0 +1,1 @@
+COPY prediction_input FROM 'prediction_input/ft/prediction_input.csv' (FORMAT 'csv', quote '"', delimiter ',', header 1);
